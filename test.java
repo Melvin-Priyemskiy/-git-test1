c7f4b3529abfd6ed7b7b92a90dc2b6b5bb8209bf
melvin <mmpriyemskiy@gmail.com>
2023-09-03 21:24:29 -0400

    update java file

--- test.java
+++ test.java
@@ -1,6 +1,5 @@
-public class main{
-    public static void main(String args[])
-    {
-        System.out.println(9*11);
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(9 * 11);
     }
 }
